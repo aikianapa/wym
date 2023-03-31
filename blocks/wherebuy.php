6fc3c63f62dc397df0f4660718b53b26b097b5b7
Oleg Frolov <oleg_frolov@mail.ru>
2023-03-31 15:42:23 +0300

    update

--- blocks/wherebuy.php
+++ blocks/wherebuy.php
@@ -10,8 +10,8 @@
                     <div class="buy-col">
                         <div class="buy-wrap">
                             <div class="buy-logo">
-                                <a href="{{wblink}}" target="_blank">
-                                    <img src="/thumbc/300x80/src{{image.0.img}}" alt="{{whname}}">
+                                <a href="{{whlink}}" target="_blank">
+                                    <img src="/thumb/300x80/src{{image.0.img}}" alt="{{whname}}">
                                 </a>
                             </div>
                             <span class="buy-name" wb-if="'{{whname}}'>''">{{whname}}</span>
@@ -29,7 +29,7 @@
     <div>
         <wb-module wb="module=yonger&mode=edit&block=common.inc" />
     </div>
-    <div class="form-group row mb-1">
+    <div class="mb-1 form-group row">
         <label class="col-form-label col-sm-4 col-xl-3">Заголовок</label>
         <div class="col-sm-8 col-xl-9">
             <input class="form-control" type="text" name="title" placeholder="Где купить">
@@ -42,20 +42,20 @@
                 <input name="image" wb="module=filepicker&mode=single&original=false&ext=png,jpg,jpeg,gif,svg,webp&width=300&height=120" wb-path="/uploads/documents/{{_parent.id}}" />
             </div>
         </div>
-        <div class="col-sm-8 pl-4 col-lg-9">
-            <div class="form-group row mb-1">
+        <div class="pl-4 col-sm-8 col-lg-9">
+            <div class="mb-1 form-group row">
                 <label class="col-form-label col-sm-3">Название</label>
                 <div class="col-sm-9">
                     <input class="form-control" type="text" name="whname">
                 </div>
             </div>
-            <div class="form-group row mb-1">
+            <div class="mb-1 form-group row">
                 <label class="col-form-label col-sm-3">Адрес</label>
                 <div class="col-sm-9">
                     <input class="form-control" type="text" name="whaddr">
                 </div>
             </div>
-            <div class="form-group row mb-1">
+            <div class="mb-1 form-group row">
                 <label class="col-form-label col-sm-3">Ссылка и телефон</label>
                 <div class="col-sm-5">
                     <input class="form-control" type="text" name="whlink" placeholder="Ссылка">
@@ -65,6 +65,6 @@
                 </div>
             </div>
         </div>
-        <hr class="col my-1">
+        <hr class="my-1 col">
     </wb-multiinput>
 </edit>
