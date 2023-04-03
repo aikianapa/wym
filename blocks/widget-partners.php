21e055edc146b3a17dbb500026487145926053c5
Oleg Frolov <oleg_frolov@mail.ru>
2023-04-03 16:33:26 +0300

    partners

--- blocks/widget-partners.php
+++ blocks/widget-partners.php
@@ -6,7 +6,7 @@
             <wb-foreach wb="from=images&tpl=false">
                 <div class="partners-item">
                     <a href="{{partlink}}" class="partners-item-wrap">
-                        <img src="/thumb/160x30/src{{image.0.img}}" alt="{{partname}}">
+                        <img src="/thumb/200x200/src{{image.0.img}}" alt="{{partname}}">
                     </a>
                 </div>
             </wb-foreach>
