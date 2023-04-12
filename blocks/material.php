8d31fdf9cadc2a843e091d805fd6a5a14556dc27
Oleg Frolov <oleg_frolov@mail.ru>
2023-04-13 00:51:29 +0300

    material pdf

--- blocks/material.php
+++ blocks/material.php
@@ -29,42 +29,48 @@
 
                 <a data-fancybox="" wb-if="'{{_parent.materials}}' == 'video' && '{{_sess.user.role}}'>''" href="{{video}}" class="link-youtube event-img" style="background-image: url({{_var.video_cover}});"></a>
                 <a data-fancybox="" wb-if="'{{_parent.materials}}' == 'video' && '{{_sess.user.role}}'==''" data-src="#modal-6" href=" #" class="link-youtube event-img" style="background-image: url({{_var.video_cover}});"></a>
+            </div>
 
-                <div class="event-row">
-                    <div class="event-sidebar">
-                        <div class="event-news" wb-if="'{{_parent.materials}}' != 'video'">
-                            <wb-foreach wb="table=materials&tpl=false&limit=1&sort=date:d" wb-filter="active=on&materials!=video&id!={{_parent.id}}">
-                                <a href="{{yongerFurl()}}" class="news-tag">{{tag}}</a>
-                                <span class="event-tit">{{header}}</span>
-                                <p>{{descr}}</p>
-                            </wb-foreach>
-                        </div>
-                        <div class="event-news" wb-if="'{{_parent.materials}}' == 'video'">
-                            <wb-foreach wb="table=materials&tpl=false&limit=1&sort=date:d" wb-filter="active=on&materials=video&id!={{_parent.id}}">
-                                <a href="{{yongerFurl()}}" class="news-tag">{{tag}}</a>
-                                <span class="event-tit">{{header}}</span>
-                                <p>{{descr}}</p>
-                            </wb-foreach>
+            <div class="event-row">
+                <div class="event-sidebar">
+                    <div class="event-news" wb-if="'{{_parent.materials}}' != 'video'">
+                        <wb-foreach wb="table=materials&tpl=false&limit=1&sort=date:d" wb-filter="active=on&materials!=video&id!={{_parent.id}}">
+                            <a href="{{yongerFurl()}}" class="news-tag">{{tag}}</a>
+                            <span class="event-tit">{{header}}</span>
+                            <p>{{descr}}</p>
+                        </wb-foreach>
+                    </div>
+                    <div class="event-news" wb-if="'{{_parent.materials}}' == 'video'">
+                        <wb-foreach wb="table=materials&tpl=false&limit=1&sort=date:d" wb-filter="active=on&materials=video&id!={{_parent.id}}">
+                            <a href="{{yongerFurl()}}" class="news-tag">{{tag}}</a>
+                            <span class="event-tit">{{header}}</span>
+                            <p>{{descr}}</p>
+                        </wb-foreach>
+                    </div>
+                </div>
+                <div class="event-content">
+                    <wb-var hide="event-text" wb-if="'{{_sess.user.role}}'==''" else="" />
+                    <div class="{{_var.hide}}">
+                        <div class="event-block">
+                            {{text}}
                         </div>
                     </div>
-                    <div class="event-content">
-                        <wb-var hide="event-text" wb-if="'{{_sess.user.role}}'==''" else="" />
-                        <div class="{{_var.hide}}">
-                            <div class="event-block">
-                                {{text}}
-                            </div>
-                        </div>
-                        <wb-include wb-tpl="allow-message.php" />
-                        <div class="event-bottom">
-                            <wb-include wb-tpl="share.php" />
-                            <div class="pagination">
-                                <a href="{{_parent.prevnext.prev}}" class="link-more"><span>Предыдущая <i>статья</i></span></a>
-                                <a href="{{_parent.prevnext.next}}" class="link-more link-more-second"><span>Следующая <i>статья</i></span></a>
-                            </div>
+                    <div class="event-content" wb-if="'{{pdf.0.img}}'>''">
+                        <iframe class="material w-100 b-0" src="{{pdf.0.img}}#toolbar=1&view=Fit" onload="iframeHeight()">
+
+                        </iframe>
+                    </div>
+                    <wb-include wb-tpl="allow-message.php" />
+                    <div class="event-bottom">
+                        <wb-include wb-tpl="share.php" />
+                        <div class="pagination">
+                            <a href="{{_parent.prevnext.prev}}" class="link-more"><span>Предыдущая <i>статья</i></span></a>
+                            <a href="{{_parent.prevnext.next}}" class="link-more link-more-second"><span>Следующая <i>статья</i></span></a>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
         </div>
     </section>
 </view>
@@ -90,6 +96,9 @@
     <div class="{{_var.hideimage}}">
         <div class="divider-text">Изображения для слайдшоу</div>
         <wb-module wb="module=filepicker&original=false&width=250&height=100" wb-path="/uploads/materials/{{_var.id}}" wb-ext="jpg,jpeg,gif,png,webp,svg" name="images">
+        </wb-module>
+        <div class="divider-text">PDF файл</div>
+        <wb-module wb="module=filepicker&original=false&width=250&height=100" wb-path="/uploads/materials/{{_var.id}}" wb-ext="pdf" name="pdf">
         </wb-module>
     </div>
 </edit>
