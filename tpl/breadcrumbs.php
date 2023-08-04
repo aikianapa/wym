d19d49a37920110ca5a14f507c8d0bd1bb33a420
Олег Фролов <oleg_frolov@mail.ru>
2023-08-04 16:12:49 +0300

    breadcrumbs schema.org

--- tpl/breadcrumbs.php
+++ tpl/breadcrumbs.php
@@ -7,22 +7,44 @@
 
 <wb-var midcrumb='{{yongerCrumbs({{_var.parent}})}}' wb-if="'{{_var.parent}}'>'/'" else='' />
 
-<ul class="crumbs" wb-if="'{{_route.direction}}'==''">
-    <li><a href="/">Главная</a></li>
+<ul class="crumbs" wb-if="'{{_route.direction}}'==''" itemscope itemtype="https://schema.org/BreadcrumbList">
+    <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+        <a itemprop="item" href="/">
+            <span itemprop="name">Главная</span>
+        </a>
+        <meta itemprop="position" content="1" />
+    </li>
     <wb-foreach wb="from=_var.midcrumb&tpl=false">
-        <li wb-if="'{{path}}' > ''">
-            <a href="{{path}}">{{header}}</a>
+        <li wb-if="'{{path}}' > ''" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+            <a itemprop="item" href="{{path}}">
+                <span itemprop="name">{{header}}</span>
+            </a>
+            <meta itemprop="position" content="{{1 + _ndx}}" />
         </li>
-        <li wb-if="'{{path}}' == ''">
-            {{header}}
+        <li wb-if="'{{path}}' == ''" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+            <span itemprop="name">{{header}}</span>
+            <meta itemprop="position" content="{{1 + _ndx}}" />
         </li>
     </wb-foreach>
 </ul>
 
-<ul class="crumbs" wb-if="'{{_route.direction}}'>''">
-    <li><a href="/">Главная</a></li>
-    <li><a href="/directions">Медицинские направления</a></li>
-    <li>{{_var.directions.{{_route.direction}}.name}}</li>
+<ul class="crumbs" wb-if="'{{_route.direction}}'>''" itemscope itemtype="https://schema.org/BreadcrumbList">
+    <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+        <a itemprop="item" href="/">
+            <span itemprop="name">Главная</span>
+        </a>
+        <meta itemprop="position" content="1" />
+    </li>
+    <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+        <a href="/directions">
+            <span itemprop="name">Медицинские направления</span>
+        </a>
+        <meta itemprop="position" content="2" />
+    </li>
+    <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+        <span itemprop="name">{{_var.directions.{{_route.direction}}.name}}</span>
+        <meta itemprop="position" content="3" />
+    </li>
 </ul>
 
 </html>
