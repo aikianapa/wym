d1d523ede7b7d694499b5064c1a04a6207b56c87
Олег Фролов <oleg_frolov@mail.ru>
2023-08-11 14:42:03 +0300

    update

---
+++ tpl/404.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+    <wb-include wb-tpl="head.php" />
+</head>
+
+<body>
+    <wb-module wb="module=yonger&mode=render&view=header" />
+    <main class="page">
+        <div class="wrapper" style="text-align:center;margin-top:100px;;margin-bottom:100px">
+            <a href="/" style="display:block;margin-top:100px;margin-bottom:50px"><img src="/assets/img/logo.svg" alt=""></a>
+            <h2>Страница не найдена</h2>
+            <p style="margin-top:50px;margin-bottom:50px">Ошибка 404</p>
+            <a href="/" class="primary-btn">Перейти на главную</a>
+        </div>
+    </main>
+    <wb-module wb="module=yonger&mode=render&view=footer" />
+</body>
+
+</html>
